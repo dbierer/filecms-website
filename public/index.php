4cd088e1c191a966466c94db651699738fbbcf02
ked <doug@unlikelysource.com>
2024-11-15 08:24:24 +0700

    2024-11-15 DB: updated edit template

--- public/index.php
+++ public/index.php
@@ -6,12 +6,14 @@ try {
     $uri  = $_POST['uri'] ?? $_SERVER['REQUEST_URI'] ?? '';
     $uri  = parse_url($uri,  PHP_URL_PATH);
     $uri  = (strlen($uri) <= 1) ? '/home' : $uri;
-    // pre-processing logic:
+    // pre-processing logic
     include SRC_DIR . '/processing.php';
     // routes w/ forms need to do an include
     $html_dir = $config['HTML_DIR'] ?? HTML_DIR;
     header('Content-Type: text/html');
     header('Content-Encoding: compress');
+    // NOTE: added $html->notFound === TRUE if requested URL is not found
+    //       you might want to add an if () statement and redirect if appropriate
     $html = new \FileCMS\Common\View\Html($config, $uri, $html_dir);
     echo $html->render();
 } catch (Throwable $t) {
